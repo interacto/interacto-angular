43a00d535ab5ec29101c921edf2e02b2c9284a92
arnobl <1052622+arnobl@users.noreply.github.com>
2021-02-25 12:02:18 +0100

    clean(directive): cleaning the clicks directive

--- projects/interacto-angular/src/lib/directives/clicks-binder.directive.ts
+++ projects/interacto-angular/src/lib/directives/clicks-binder.directive.ts
@@ -1,22 +1,34 @@
 import {Directive, ElementRef, Input, ViewContainerRef} from '@angular/core';
 import {clicksBinder} from 'interacto';
 
+/**
+ * The clicks directive uses this data structure for starting the
+ * configuration of the binder.
+ */
+export interface ClicksDirectiveData {
+  /**
+   * The expected number of clicks.
+   */
+  count: number;
+
+  /**
+   * The function of the component that will be called to configure the binding.
+   */
+  fn: string;
+}
+
 @Directive({
   selector: '[ioClicks]'
 })
 export class ClicksBinderDirective {
-  @Input() count: number;
-  @Input() method: string;
-
-  constructor(private element: ElementRef,
-              private viewContainerRef: ViewContainerRef) {
+  constructor(private element: ElementRef, private viewContainerRef: ViewContainerRef) {
   }
 
   @Input()
-  set ioClicks(data: {'count': number, 'method': string}) {
+  set ioClicks(data: ClicksDirectiveData) {
     const partialBinder = clicksBinder(data.count)
       .on(this.element.nativeElement);
 
-    (this.viewContainerRef as any)._view.component[data.method](partialBinder);
+    (this.viewContainerRef as any)._view.component[data.fn](partialBinder);
   }
 }
