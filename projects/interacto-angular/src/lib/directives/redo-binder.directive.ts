1274866b4750f65d425d69f42d1c69efadaee8d0
arnobl <1052622+arnobl@users.noreply.github.com>
2021-03-01 15:06:15 +0100

    feat(directive): the undo and redo directives also update the text and the disable property of the button

--- projects/interacto-angular/src/lib/directives/redo-binder.directive.ts
+++ projects/interacto-angular/src/lib/directives/redo-binder.directive.ts
@@ -1,15 +1,39 @@
-import {Directive, ElementRef} from '@angular/core';
+import {Directive, ElementRef, OnDestroy} from '@angular/core';
 import {Bindings, Redo} from 'interacto';
+import {Subscription} from 'rxjs';
 
 @Directive({
   selector: '[ioRedo]'
 })
-export class RedoBinderDirective {
-  constructor(element: ElementRef, bindings: Bindings) {
+export class RedoBinderDirective implements OnDestroy {
+  private readonly redoObs: Subscription;
+
+  constructor(private element: ElementRef, private bindings: Bindings) {
+    if (!(element.nativeElement instanceof HTMLButtonElement)) {
+      throw new Error('The redo directive only applies on buttons');
+    }
+
+    this.redoObs = bindings.undoHistory
+      .redosObservable()
+      .subscribe(_ => {
+        this.updateRedo();
+      });
+
+    this.updateRedo();
+
     bindings
       .buttonBinder()
       .on(element)
       .toProduce(() => new Redo(bindings.undoHistory))
       .bind();
+  }
+
+  private updateRedo(): void {
+    this.element.nativeElement.disabled = this.bindings.undoHistory.getLastRedo() === undefined;
+    this.element.nativeElement.title = this.bindings.undoHistory.getLastOrEmptyRedoMessage();
+  }
+
+  ngOnDestroy(): void {
+    this.redoObs.unsubscribe();
   }
 }
--- projects/interacto-angular/src/lib/directives/undo-binder.directive.ts
+++ projects/interacto-angular/src/lib/directives/undo-binder.directive.ts
@@ -1,15 +1,39 @@
-import {Directive, ElementRef} from '@angular/core';
+import {Directive, ElementRef, OnDestroy} from '@angular/core';
 import {Bindings, Undo} from 'interacto';
+import {Subscription} from 'rxjs';
 
 @Directive({
   selector: '[ioUndo]'
 })
-export class UndoBinderDirective {
-  constructor(element: ElementRef, bindings: Bindings) {
+export class UndoBinderDirective implements OnDestroy {
+  private readonly undoObs: Subscription;
+
+  constructor(private element: ElementRef, private bindings: Bindings) {
+    if (!(element.nativeElement instanceof HTMLButtonElement)) {
+      throw new Error('The undo directive only applies on buttons');
+    }
+
+    this.undoObs = bindings.undoHistory
+      .undosObservable()
+      .subscribe(_ => {
+        this.updateUndo();
+      });
+
+    this.updateUndo();
+
     bindings
       .buttonBinder()
       .on(element)
       .toProduce(() => new Undo(bindings.undoHistory))
       .bind();
+  }
+
+  private updateUndo(): void {
+    this.element.nativeElement.disabled = this.bindings.undoHistory.getLastUndo() === undefined;
+    this.element.nativeElement.title = this.bindings.undoHistory.getLastOrEmptyUndoMessage();
+  }
+
+  ngOnDestroy(): void {
+    this.undoObs.unsubscribe();
   }
 }
