afcf5c1f2af084f38cbe9d7e4df1f32aace130b0
Rémi Daniel <remidaniel44@yahoo.fr>
2021-06-22 11:00:57 +0200

    change(directive): the ClicksBinder directive now uses several parameters.

--- projects/interacto-angular/src/lib/directives/clicks-binder.directive.ts
+++ projects/interacto-angular/src/lib/directives/clicks-binder.directive.ts
@@ -1,21 +1,5 @@
-import {Directive, ElementRef, Input, ViewContainerRef} from '@angular/core';
-import {Bindings} from 'interacto';
-
-/**
- * The clicks directive uses this data structure for starting the
- * configuration of the binder.
- */
-export interface ClicksDirectiveData {
-  /**
-   * The expected number of clicks.
-   */
-  count: number;
-
-  /**
-   * The function of the component that will be called to configure the binding.
-   */
-  fn: string;
-}
+import {ChangeDetectorRef, Directive, ElementRef, Input, ViewContainerRef} from '@angular/core';
+import {Bindings, PartialPointsBinder} from 'interacto';
 
 @Directive({
   selector: '[ioClicks]'
@@ -23,12 +7,25 @@ export interface ClicksDirectiveData {
 export class ClicksBinderDirective {
   constructor(private element: ElementRef,
               private viewContainerRef: ViewContainerRef,
+              private changeDetectorRef: ChangeDetectorRef,
               private bindings: Bindings) {
   }
 
+  /**
+   * The expected number of clicks.
+   */
   @Input()
-  set ioClicks(data: ClicksDirectiveData) {
-    const partialBinder = this.bindings.clicksBinder(data.count).on(this.element);
-    (this.viewContainerRef as any)._hostLView[8][data.fn](partialBinder);
+  count = 2;
+
+  /**
+   * Starts the creation of a ClicksBinder.
+   * @param fn - The function of the component that will be called to configure the binding.
+   */
+  @Input()
+  set ioClicks(fn:
+                   (partialBinder: PartialPointsBinder | undefined) => void)  {
+    this.changeDetectorRef.detectChanges(); // Detects changes to the component and retrieves the value for count
+    const partialBinder = this.bindings.clicksBinder(this.count).on(this.element);
+    (this.viewContainerRef as any)._hostLView[8][fn.name](partialBinder);
   }
 }
