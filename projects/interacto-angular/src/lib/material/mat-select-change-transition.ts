dbfc70afd071343657c70f59d3205aae361df531
arnobl <1052622+arnobl@users.noreply.github.com>
2023-06-11 22:54:07 +0200

    update(ng): moving to Angular 15

--- projects/interacto-angular/src/lib/material/mat-select-change-transition.ts
+++ projects/interacto-angular/src/lib/material/mat-select-change-transition.ts
@@ -3,6 +3,9 @@ import {MatSelectChange} from '@angular/material/select';
 import {MatEvent} from './mat-event';
 
 export class MatSelectChangeTransition extends TransitionBase<MatEvent<MatSelectChange>> {
+  //FIXME: remove as
+  private static events: ReadonlySet<EventType> = new Set(["material" as EventType]);
+
   public constructor(srcState: OutputState, tgtState: InputState,
                      action: (evt: MatEvent<MatSelectChange>) => void, guard?: (evt: MatEvent<MatSelectChange>) => boolean) {
     super(srcState, tgtState, action, guard);
@@ -12,8 +15,7 @@ export class MatSelectChangeTransition extends TransitionBase<MatEvent<MatSelect
     return event instanceof MatEvent && event.change instanceof MatSelectChange;
   }
 
-  public getAcceptedEvents(): ReadonlyArray<EventType> {
-    //FIXME: remove as
-    return ["material" as EventType];
+  public getAcceptedEvents(): ReadonlySet<EventType> {
+    return MatSelectChangeTransition.events;
   }
 }
